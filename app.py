69aefb88d4a4b74230392d7e0142fe6f06c86608
tracy su <tracysu@tracys-MacBook-Pro.local>
2020-01-22 13:44:37 -0500

    customize swagger ui

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wfadnpqa:dwGPR7uApefy9_yEGKrMyY9as9-z8LVv@rajje.db.elephantsql.com:5432/wfadnpqa'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'tracysuproject'
+app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
 
 db = SQLAlchemy(app)
 
